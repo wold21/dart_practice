4da2a8927a6fe2c2696a170e3b1efe99066c1c30
wold21 <wold2180@gmail.com>
2021-09-12 15:50:02 +0900

    String

---
+++ 2H_recture/string.dart
@@ -0,0 +1,25 @@
+main() {
+  var s1 = 'Contrary to popular belief, Lorem Ipsum is not simply random text.';
+  var s2 =
+      "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.";
+  var s3 =
+      '\'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,';
+  var s4 =
+      "'consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.\n";
+  // print(s1);
+  // print(s2);
+  // print(s3);
+  // print(s4);
+
+  // RAW String
+  var s =
+      r'Contrary to popular belief, \n Lorem Ipsum is not simply random text.';
+  // print(s);
+
+  var age = 27;
+  var str = 'My age is: $age';
+  // print(str);
+
+  // Multi Line
+  // ''', """ 가능
+}
